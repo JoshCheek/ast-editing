9e4d50f900a7cb600e76ac2aa9528e80481db2d4
Josh Cheek <josh.cheek@gmail.com>
2018-08-04 11:35:38 -0500

    Ecma: Copy Ruby's styles, implement import

--- src/RenderEcma.js
+++ src/RenderEcma.js
@@ -1,12 +1,26 @@
-import React, { Component } from 'react';
-// import './ecma_syntax.css';
+import React, { Component } from 'react'
+import './syntax_ecma.css'
+
+class Kw extends Component {
+  render() {
+    return <span className="keyword">{this.props.children}</span>
+  }
+}
+class Chunk extends Component {
+  render() {
+    return <span className="displayBlock">{this.props.children}</span>
+  }
+}
 
 class RenderEcma extends Component {
+  className(ast, classes) {
+    return [...classes, 'Ast', ast.type.slice(3)].join(" ")
+  }
   render(args) {
-    return <p>pending</p>
-    // return <div className="Ecma">
-    //   {this.renderAst(this.props.ast, [], 0)}
-    // </div>
+    // return <p>pending</p>
+    return <div className="Ecma">
+      {this.renderAst(this.props.ast, [], 0)}
+    </div>
   }
   renderAst(ast, classes, key) {
     if(!ast) return null
@@ -15,6 +29,28 @@ class RenderEcma extends Component {
     if(handlerName in this)
       return this[handlerName](ast, classes, key)
     throw new Error(`No AST handler "${handlerName}" for ${ast.type} syntax!`)
+  }
+  renderAstBegin(ast, classes, key) {
+    return <span className={this.className(ast, classes)} key={key}>
+      {ast.map((child, i) => this.renderAst(child, [], i))}
+    </span>
+  }
+  renderAstImport(ast, classes, key) {
+    return <span className={this.className(ast, classes)} key={key}>
+      <Kw>import</Kw>
+      {this.renderAst(ast.name, ['name'], 0)}
+      <Kw>from</Kw>
+      {this.renderAst(ast.location, ['location'], 1)}
+    </span>
+  }
+  renderAstConstant(ast, classes, key) {
+    return "constant"
+  }
+  renderAstString(ast, classes, key) {
+    return "string"
+  }
+  renderAstClass(ast, classes, key) {
+    return "class"
   }
 }
 
