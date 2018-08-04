4a48ce0df4784061f8e99bb2f8e53c7528541662
Josh Cheek <josh.cheek@gmail.com>
2018-08-04 01:36:16 -0500

    First pass
    Based on https://github.com/JoshCheek/seeing_is_believing/blob/c8d7fa04ae218c49d654f312a34fc2627a2b2749/lib/seeing_is_believing/event_stream/handlers/record_exit_events.rb

--- src/index.js
+++ src/index.js
@@ -1,8 +1,103 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Ruby extends Component {
+  render() {
+    return this.renderAst(this.props.ast, [], 0)
+  }
+
+  renderAst(ast, classes, key) {
+    if(!ast) return null
+    if(typeof ast === 'string') return ast
+    return <div className={ast.className(classes)} key={key}>
+      {ast.children.map(
+        (child, i) => this.renderAst(child, ast.childClasses[i], i)
+      )}
+    </div>
+  }
+
+}
+
+class Ast {
+  constructor(...children) {
+    this.children = children
+  }
+  get type()         { return 'generic' }
+  get childClasses() { return new Array(this.children.length).fill("") }
+  get isRoot()       { return false }
+  get isLiteral()    { return false }
+  get isAst()        { return true }
+  className(classes) {
+    return [...classes, this.constructor.name.slice(3)].join(" ")
+  }
+}
+class AstBegin extends Ast {
+  get type() { return 'begin' }
+}
+class AstLiteral extends Ast {
+  get isLiteral() { return true }
+}
+class AstString extends AstLiteral {
+  get type() { return 'string' }
+}
+class AstSymbol extends AstLiteral {
+  get type() { return 'string' }
+}
+class AstCall extends Ast {
+  get type()         { return 'call' }
+  get childClasses() { return ['receiver', 'name', 'args'] }
+}
+class AstClass extends Ast {
+  get type()         { return 'class' }
+  get childClasses() { return ['name', 'superclass', 'body'] }
+}
+class AstModule extends Ast {
+  get type()         { return 'class' }
+  get childClasses() { return ['name', 'body'] }
+}
+class AstConstant extends Ast {
+  get childClasses() { return ['namespace', 'name'] }
+}
+
+
+const ast = new AstBegin(
+  new AstCall(
+    null,
+    "require",
+    new AstString("seeing_is_believing/event_stream/events")
+  ),
+  new AstClass(
+    new AstConstant(null, "SeeingIsBelieving"),
+    null,
+    new AstModule(
+      new AstConstant(null, "EventStream"),
+      new AstModule(
+        new AstConstant(null, 'Handlers'),
+        new AstClass(
+          new AstConstant(null, 'RecordExitEvents'),
+          null,
+          new AstBegin(
+            new AstCall(null, 'attr_reader', new AstSymbol('exitstatus')),
+            new AstCall(null, 'attr_reader', new AstSymbol('timeout_seconds')),
+          ),
+        ),
+      ),
+    ),
+  ),
+)
+const position = []
+
+renderUpdate()
+function renderUpdate() {
+  ReactDOM.render(
+    <div>
+      <Ruby ast={ast}/>
+    </div>,
+    document.getElementById('root')
+  )
+}
+
 registerServiceWorker();
