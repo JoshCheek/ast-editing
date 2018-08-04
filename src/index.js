9c2d23381fb1621fd9e3325dd6b1e96da7a51dff
Josh Cheek <josh.cheek@gmail.com>
2018-08-04 16:18:14 -0500

    Navigate with vim keybindings

--- src/index.js
+++ src/index.js
@@ -6,39 +6,96 @@ import './index.css'
 import registerServiceWorker from './registerServiceWorker'
 import Ast, {exampleAst} from './Ast.js'
 
-function setCursor(state) {
-  let {ast, position} = state
+const commands = {
+  in:   'in',
+  out:  'out',
+  down: 'down',
+  up:   'up',
+}
+
+function boundPosition(ast, position) {
   if(!position.length)
-    return {ast: Ast.AstSelected(ast), position: position}
+    return []
+  if(!ast)
+    return []
+  let [crnt, ...rest] = position
+  if(crnt < 0)
+    return [ast.length-1]
+  if(ast.length <= crnt)
+    return [0]
+  position = boundPosition(ast[crnt], rest)
+  position.unshift(crnt)
+  return position
+}
+
+function applyCommand({ast, selectedAst, position}, command) {
+  position = [...position]
+  switch (command) {
+    case commands.in:
+      position.push(0)
+      break;
+    case commands.out:
+      position.pop()
+      break;
+    case commands.up:
+      if(position.length) {
+        const index = position.length-1
+        position[index] = position[index]-1
+      }
+      break
+    case commands.down:
+      if(position.length) {
+        const index = position.length-1
+        position[index] = position[index]+1
+      }
+      break
+  }
+  position    = boundPosition(ast, position)
+  selectedAst = selectPosition(ast, position)
+  return {ast, selectedAst, position}
+}
+
+function selectPosition(ast, position) {
+  if(!position.length)
+    return Ast.AstSelected(ast)
   const [index, ...rest] = position
   ast = ast.dup()
-  const result = setCursor({ast: ast[index], position: rest})
-  ast[index] = result.ast
-  return {ast, position: [index, ...result.position]}
+  ast[index] = selectPosition(ast[index], rest)
+  return ast
 }
 
 
-let state = setCursor({
-  ast: exampleAst,
-  position: [1, 2, 1, 1, 2, 2, 2]
-})
+let state = {ast: exampleAst, position: []}
+state.selectedAst = state.ast
+state = applyCommand(state, commands.in)
+window.state = state
 
 const onKeyPress = function(event) {
-  console.log(event)
-  if(event.key === 'Enter') {
-    // *shrug* couldn't think of a good way to do it nondestructively, so, fuckit!
-    state.position.pop()
-    state = setCursor(state)
-    renderUpdate()
+  switch (event.key) {
+    case 'h':
+      state = applyCommand(state, commands.out)
+      break;
+    case 'l':
+      state = applyCommand(state, commands.in)
+      break;
+    case 'j':
+      state = applyCommand(state, commands.down)
+      break;
+    case 'k':
+      state = applyCommand(state, commands.up)
+      break;
   }
+  window.state = state
+  renderUpdate()
 }
+document.addEventListener('keypress', onKeyPress)
 
 renderUpdate()
 function renderUpdate() {
   ReactDOM.render(
-    <div onKeyPress={onKeyPress} tabIndex="0">
-      <RenderRuby ast={state.ast}/>
-      <RenderEcma ast={state.ast}/>
+    <div>
+      <RenderRuby ast={state.selectedAst}/>
+      <RenderEcma ast={state.selectedAst}/>
     </div>,
     document.getElementById('root')
   )
